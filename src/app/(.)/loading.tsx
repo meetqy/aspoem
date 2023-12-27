ecb7eff09a0d8d22a05185fdf949cd9f6d171a25
meetqy <meetqy@qq.com>
2023-12-27 17:40:52 +0800

    loading

---
+++ src/app/(.)/loading.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { useSearchParams } from "next/navigation";
+
+export default function Loading() {
+  // 简写 loading text => lt
+  const params = useSearchParams();
+  const title = params.get("lt") ?? "Let's Go !";
+
+  return (
+    <div className="flex h-full w-full items-center justify-center uppercase">
+      <motion.div className="text-center">
+        {title.split("").map((item, index) => (
+          <motion.span
+            initial={{ opacity: 0 }}
+            animate={{
+              opacity: 100,
+            }}
+            transition={{ delay: index * 0.025 }}
+            className="text-stroke-base-100 text-9xl"
+            key={index}
+          >
+            {item}
+          </motion.span>
+        ))}
+      </motion.div>
+    </div>
+  );
+}
--- src/app/_components/CardItem/index.tsx
+++ src/app/_components/CardItem/index.tsx
@@ -10,7 +10,7 @@ export default function CardItem(props: Props) {
   const first = props.text[0];
 
   return (
-    <Link href={props.href} className={styles.card}>
+    <Link href={`${props.href}?lt=${props.text}`} className={styles.card}>
       <span className={`text-stroke-base-100 ${styles.title}`}>{first}</span>
 
       <p className="w-full truncate bg-neutral/90 px-2 py-3 text-neutral-content">
