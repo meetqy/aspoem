10e308d6af4c6afaacfaad0e770866c567a92ef3
meetqy <meetqy@qq.com>
2024-01-03 14:52:53 +0800

    robot sitemap

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,11 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: ["/create/", "/demo/"],
     },
-    sitemap: "https://aspoem.com/sitemap.xml",
+    sitemap: [
+      "https://aspoem.com/sitemap.xml",
+      "https://aspoem.com/author/sitemap.xml",
+      "https://aspoem.com/poem/sitemap.xml",
+      "https://aspoem.com/list/sitemap.xml",
+    ],
   };
 }
