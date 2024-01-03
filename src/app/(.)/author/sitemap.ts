f08a323ced8320f6716f28c55e334b057ffe1adb
meetqy <meetqy@qq.com>
2024-01-03 22:38:46 +0800

    sitemap author

--- src/app/(.)/author/sitemap.ts
+++ src/app/(.)/author/sitemap.ts
@@ -1,12 +1,11 @@
 import { type MetadataRoute } from "next";
 import { api } from "~/trpc/server";
-import { getBaseUrl } from "~/trpc/shared";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const data = await api.author.sitemap.query();
 
   return data.map((item) => ({
-    url: `${getBaseUrl()}/poem/${item.id}`,
+    url: `https://aspoem.com/poem/poem/${item.id}`,
     lastModified: new Date(item.updatedAt ?? "2024-01-01"),
   }));
 }
