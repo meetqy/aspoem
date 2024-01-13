421141ee1f9780e86a6967b664b1f82f1ed1c48b
meetqy <meetqy@qq.com>
2024-01-13 18:43:40 +0800

    test twitter card

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,11 +5,13 @@ import { TRPCReactProvider } from "~/trpc/react";
 import { cn } from "~/utils";
 
 import Script from "next/script";
+import { type Metadata } from "next";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "学习中国古诗词 - aspoem.com",
   description: `让我们一起来学习中国古诗词吧！快速查找诗词，拼音标注，现代化网页设计便于阅读。`,
   icons: [{ rel: "icon", url: "/favicon.ico" }],
+  keywords: ["中国古诗词", "拼音", "学习", "诗词", "古诗词", "aspoem.com"],
 };
 
 export default function RootLayout({
@@ -24,7 +26,6 @@ export default function RootLayout({
           "min-h-screen bg-background font-cursive text-foreground antialiased",
         )}
       >
-        {/* <div className="bg-patterns-wavy fixed z-0 h-screen w-screen"></div> */}
         <TRPCReactProvider>
           <ThemeProvider
             attribute="class"
---
+++ src/app/opengraph-image.tsx
@@ -0,0 +1,36 @@
+import { ImageResponse } from "next/og";
+
+// Route segment config
+export const runtime = "edge";
+
+// Image metadata
+export const alt = "学习中国古诗词 - aspoem.com";
+export const size = {
+  width: 1200,
+  height: 630,
+};
+
+export const contentType = "image/png";
+
+// Image generation
+export default async function Image() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          background: "white",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <span style={{ fontSize: 156, color: "#000" }}>学习中国古诗词</span>
+        <span style={{ fontSize: 128, color: "#555" }}>aspoem.com</span>
+      </div>
+    ),
+    size,
+  );
+}
