3b8547e4653333ecf440acb4ef836a298c22efe4
meetqy <meetqy@qq.com>
2024-01-16 17:31:47 +0800

    author ui

--- src/app/(.)/author/list/[page]/page.tsx
+++ src/app/(.)/author/list/[page]/page.tsx
@@ -1,6 +1,7 @@
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import Link from "next/link";
 import { notFound } from "next/navigation";
+import { Badge } from "~/components/ui/badge";
 import { Button } from "~/components/ui/button";
 import { HeaderMain } from "~/components/ui/header";
 import { api } from "~/trpc/server";
@@ -31,53 +32,39 @@ export default async function AuthorPage({
           <span className="text-2xl font-bold">诗人</span>
         </div>
       </HeaderMain>
-      <div className="m-auto grid max-w-screen-md grid-cols-1 gap-4 py-4">
+      <div className="m-auto grid grid-cols-2 gap-4 py-4">
         {data?.map((item) => (
           <div
+            className="relative flex h-36 cursor-pointer flex-col justify-between rounded-md border border-border p-4 transition-all hover:bg-muted hover:shadow-lg"
             key={item.id}
-            className="group relative cursor-pointer overflow-hidden rounded-md border border-border p-8 transition-all hover:bg-accent hover:shadow-lg"
           >
             <Link
               href={`/author/${item.id}`}
               className="absolute left-0 top-0 h-full w-full"
-            />
-            <div className="flex items-center">
-              <div className="text-outline flex h-36 w-36 flex-shrink-0 items-center justify-center overflow-hidden rounded-full bg-gradient-to-tl from-muted to-muted/20 text-8xl uppercase group-hover:bg-background">
-                <span className="-mt-2 opacity-30">{item.name[0]}</span>
-              </div>
-              <div className="flex-1 pl-8">
-                <h1 className="text-5xl font-bold uppercase text-foreground">
-                  {item.name}
-                  <span className="ml-2 font-mono text-lg font-normal capitalize text-foreground/80">
-                    {item.namePinYin}
-                  </span>
-                </h1>
-                <p className="mt-2 line-clamp-2 text-muted-foreground">
-                  {item.introduce || "暂未完善"}
-                </p>
-              </div>
-            </div>
-
-            <div className="my-8 flex justify-center">
-              <div className="h-[1px] w-36 bg-muted"></div>
-            </div>
-
-            <div className="grid w-full grid-cols-3">
-              <div className="flex flex-col items-center justify-center border-r border-border/40">
-                <span className="font-mono text-2xl font-bold text-blue-500">
-                  {item._count.poems}
-                </span>
-                <span className="text-sm text-muted-foreground">作品</span>
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <span className="font-mono text-2xl font-bold">
-                  {item.birthDate && item.deathDate
+            ></Link>
+            <div className="flex w-full items-center justify-between text-4xl font-bold">
+              {item.name}
+              <div className="flex space-x-2 font-mono">
+                <Badge
+                  variant={"destructive"}
+                  className="flex aspect-square w-10 items-center justify-center text-sm"
+                >
+                  {item?._count.poems}
+                </Badge>
+                <Badge
+                  variant={"secondary"}
+                  className="flex aspect-square w-10 items-center justify-center text-sm"
+                >
+                  {item?.birthDate && item.deathDate
                     ? item.deathDate - item.birthDate
                     : "?"}
-                </span>
-                <span className="text-sm text-muted-foreground">享年</span>
+                </Badge>
               </div>
             </div>
+
+            <p className="line-clamp-2 text-muted-foreground">
+              {item.introduce || "暂未完善"}
+            </p>
           </div>
         ))}
       </div>
---
+++ src/app/create/table/page.tsx
@@ -0,0 +1,125 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { Button } from "~/components/ui/button";
+import { api } from "~/trpc/react";
+
+type C = {
+  title: string;
+  content: string;
+  author: string;
+  tags: string[];
+};
+
+export default function Page() {
+  const searchParams = useSearchParams();
+  const [dataSource, setDataSource] = useState<C[]>();
+  const utils = api.useUtils();
+
+  const author = api.author.create.useMutation();
+  const poem = api.poem.create.useMutation();
+
+  const [save, setSave] = useState<string[]>([]);
+
+  useEffect(() => {
+    setSave(JSON.parse(localStorage.getItem("save") || "[]") as string[]);
+  }, []);
+
+  useEffect(() => {
+    void fetch("/content.json")
+      .then((res) => res.json())
+      .then((res) => {
+        setDataSource(
+          (res as C[]).filter((item) => !save.includes(item.title)),
+        );
+      });
+  }, [save]);
+
+  const { data } = api.author.findMany.useQuery({ pageSize: 999 });
+  const authorNames = data?.data.map((item) => item.name) ?? [];
+
+  const add = (item: C) => {
+    if (!authorNames.includes(item.author)) {
+      author
+        .mutateAsync({
+          name: item.author,
+          dynasty: "唐",
+          token: searchParams.get("token") ?? "",
+        })
+        .then((e) => {
+          addPoem(item, e.id);
+          void utils.author.invalidate();
+        })
+        .catch((e) => {
+          console.error("author", e);
+          alert(e);
+        });
+    } else {
+      const authorId = data?.data.find((e) => e.name === item.author)?.id;
+      if (authorId) {
+        addPoem(item, authorId);
+      }
+    }
+  };
+
+  useEffect(() => {
+    setDataSource((dataSource) => {
+      if (!dataSource) return;
+      return dataSource.filter((item) => !save.includes(item.title));
+    });
+
+    localStorage.setItem("save", JSON.stringify(save));
+  }, [save]);
+
+  const addPoem = (item: C, authorId: number) => {
+    poem
+      .mutateAsync({
+        title: item.title,
+        content: item.content,
+        authorId,
+        token: searchParams.get("token") ?? "",
+      })
+      .then((e) => {
+        setSave([...save, e.title]);
+      })
+      .catch((e: Error) => {
+        if (e.message.includes("已存在")) {
+          setSave([...save, item.title]);
+          return;
+        }
+
+        console.error("poem", e);
+        alert(e);
+      });
+  };
+
+  return (
+    <div>
+      <header className="flex h-12">
+        <div className="w-20">操作</div>
+        <div className="w-20">index</div>
+        <div className="w-20">author</div>
+        <div className="w-52">title</div>
+        <div className="w-2/5">tags</div>
+        <div className="flex-1">content</div>
+      </header>
+      <main>
+        {dataSource?.map((item, i) => (
+          <div className="flex h-12 border-b" key={i}>
+            <div className="w-20">
+              <Button variant={"outline"} size={"sm"} onClick={() => add(item)}>
+                添加
+              </Button>
+            </div>
+            <div className="w-20">{i + 1}</div>
+            <div className="w-20">{item.author}</div>
+            <div className="w-52">{item.title}</div>
+            <div className="w-2/5">{item.tags.join(",")}</div>
+            <div className="line-clamp-2 flex-1">{item.content}</div>
+          </div>
+        ))}
+      </main>
+    </div>
+  );
+}
