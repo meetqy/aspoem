54e2049c5e1175b50fd8361111ea8b0f2b1665c6
meetqy <meetqy@qq.com>
2024-01-18 11:00:56 +0800

    移除评论

--- src/app/(.)/poem/[id]/page.tsx
+++ src/app/(.)/poem/[id]/page.tsx
@@ -6,7 +6,7 @@ import { api } from "~/trpc/server";
 import PinYinText from "./components/PinYinText";
 import { type Metadata } from "next";
 import { cache } from "react";
-import dynamic from "next/dynamic";
+// import dynamic from "next/dynamic";
 import { Button } from "~/components/ui/button";
 import { cn } from "~/utils";
 
@@ -15,9 +15,9 @@ type Props = {
   searchParams: { py?: string };
 };
 
-const MyGiscus = dynamic(() => import("./components/my-giscus"), {
-  ssr: false,
-});
+// const MyGiscus = dynamic(() => import("./components/my-giscus"), {
+//   ssr: false,
+// });
 
 const getItem = cache(async (id: string) => {
   const poem = await api.poem.findById.query(Number(id));
@@ -186,9 +186,9 @@ export default async function Page({ params, searchParams }: Props) {
           发现错误，也可以在下方留言，指正哦！
         </p>
 
-        <div className="mt-12">
+        {/* <div className="mt-12">
           <MyGiscus />
-        </div>
+        </div> */}
       </article>
 
       <footer className="h-16"></footer>
