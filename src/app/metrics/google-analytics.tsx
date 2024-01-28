19bb54beb51b36ca7cae7c6cb40c9325b437d421
meetqy <meetqy@qq.com>
2024-01-28 12:14:46 +0800

    文档补充

---
+++ src/app/metrics/google-analytics.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import Script from "next/script";
+
+const GoogleAnalytics = ({ id }: { id: string }) => {
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${id}`}
+      />
+      <Script
+        id="google-analytics-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', '${id}');
+                    `,
+        }}
+      />
+    </>
+  );
+};
+
+export default GoogleAnalytics;
