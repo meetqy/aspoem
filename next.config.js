3f4bbbacd554206212fb928988345cc7a1d2b5f8
meetqy <meetqy@qq.com>
2024-02-02 11:26:06 +0800

    unoptimized image

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default config;
