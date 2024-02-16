f0bf2a11adad03edc90b8619577df312140d7ead
meetqy <meetqy@qq.com>
2024-02-16 16:55:07 +0800

    button sm 高度改为 32 h-8

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -21,7 +21,7 @@ const buttonVariants = cva(
       },
       size: {
         default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
+        sm: "h-8 rounded-md px-3",
         xs: "px-3 py-1 rounded-md",
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
