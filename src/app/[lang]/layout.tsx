b8c45e5ed32b111b8ebbfc952306a1feae0d9a1f
meetqy <meetqy@qq.com>
2024-03-01 10:48:44 +0800

    结构

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -46,48 +46,46 @@ export default async function Layout({
   const dict = await getDictionary(params.lang);
 
   return (
-    <html lang={params.lang}>
-      <Root>
-        <div className="relative z-10 flex bg-background/20 p-0">
-          <aside className="hidden lg:block">
-            <div className="fixed left-0 top-0 h-screen bg-muted/50">
-              <ScrollArea className="sticky top-0 h-full w-72 border-border">
-                <header className="h-16">
-                  <Link
-                    href={`/${params.lang}`}
-                    className="text-outline flex h-16 items-center justify-center font-serif text-[2.5rem] font-bold"
-                  >
-                    AsPoem
-                    <span className="text-muted-foreground">.com</span>
-                  </Link>
-                </header>
+    <Root lang={params.lang}>
+      <div className="relative z-10 flex bg-background/20 p-0">
+        <aside className="hidden lg:block">
+          <div className="fixed left-0 top-0 h-screen bg-muted/50">
+            <ScrollArea className="sticky top-0 h-full w-72 border-border">
+              <header className="h-16">
+                <Link
+                  href={`/${params.lang}`}
+                  className="text-outline flex h-16 items-center justify-center font-serif text-[2.5rem] font-bold"
+                >
+                  AsPoem
+                  <span className="text-muted-foreground">.com</span>
+                </Link>
+              </header>
 
-                <div>
-                  <DesktopMenu dict={dict} lang={params.lang} />
-                </div>
-              </ScrollArea>
-            </div>
-            <div className="w-72"></div>
-          </aside>
-
-          <MobileMenu dict={dict} lang={params.lang} />
-
-          <div className="flex-1 bg-gradient-to-t from-background to-muted/10">
-            <header
-              className="sticky top-0 z-40 flex min-h-16 w-full flex-row-reverse items-center justify-between border-b border-border/40 bg-gradient-to-b from-background to-muted/40 pl-14 pr-4 backdrop-blur lg:pl-0"
-              id="header_main"
-            >
-              <div className="flex items-center justify-center">
-                <div className="mr-2 hidden lg:block">
-                  <Search dict={dict} lang={params.lang} />
-                </div>
-                <ModeToggle />
+              <div>
+                <DesktopMenu dict={dict} lang={params.lang} />
               </div>
-            </header>
-            <main className="relative m-auto max-w-screen-md">{children}</main>
+            </ScrollArea>
           </div>
+          <div className="w-72"></div>
+        </aside>
+
+        <MobileMenu dict={dict} lang={params.lang} />
+
+        <div className="flex-1 bg-gradient-to-t from-background to-muted/10">
+          <header
+            className="sticky top-0 z-40 flex min-h-16 w-full flex-row-reverse items-center justify-between border-b border-border/40 bg-gradient-to-b from-background to-muted/40 pl-14 pr-4 backdrop-blur lg:pl-0"
+            id="header_main"
+          >
+            <div className="flex items-center justify-center">
+              <div className="mr-2 hidden lg:block">
+                <Search dict={dict} lang={params.lang} />
+              </div>
+              <ModeToggle />
+            </div>
+          </header>
+          <main className="relative m-auto max-w-screen-md">{children}</main>
         </div>
-      </Root>
-    </html>
+      </div>
+    </Root>
   );
 }
--- src/app/root.tsx
+++ src/app/root.tsx
@@ -6,6 +6,7 @@ import { TRPCReactProvider } from "~/trpc/react";
 
 import dynamic from "next/dynamic";
 import LoadFont from "./load-font";
+import { type Locale } from "./[lang]/dictionaries";
 
 const MicrosoftClarity = dynamic(() => import("./metrics/microsoft-clarity"), {
   ssr: false,
@@ -15,10 +16,16 @@ const GoogleAnalytics = dynamic(() => import("./metrics/google-analytics"), {
   ssr: false,
 });
 
-export default function Root({ children }: { children: React.ReactNode }) {
+export default function Root({
+  children,
+  lang,
+}: {
+  children: React.ReactNode;
+  lang: Locale;
+}) {
   return (
-    <body className="theme-zinc min-h-screen bg-background font-cursive font-normal text-foreground antialiased">
-      <>
+    <html lang={lang}>
+      <body className="theme-zinc min-h-screen bg-background font-cursive font-normal text-foreground antialiased">
         <TRPCReactProvider>
           <ThemeProvider
             attribute="class"
@@ -33,7 +40,7 @@ export default function Root({ children }: { children: React.ReactNode }) {
         <GoogleAnalytics id={process.env.NEXT_PUBLIC_GA_ID!} />
         <MicrosoftClarity id={process.env.NEXT_PUBLIC_MC_ID!} />
         <LoadFont />
-      </>
-    </body>
+      </body>
+    </html>
   );
 }
