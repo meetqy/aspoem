b9fbfa2c976af45a81f9befd766e957f0dbf3c3a
meetqy <meetqy@qq.com>
2024-03-22 20:28:24 +0800

    打印

--- src/app/tools/print/components/toggle-option.tsx
+++ src/app/tools/print/components/toggle-option.tsx
@@ -35,15 +35,15 @@ export const ToggleOption = () => {
   }, [opts, router, id]);
 
   return (
-    <div>
+    <div className="space-y-2 text-f50">
       <p className="mb-2 text-muted-foreground">自定义打印内容</p>
       <div className="flex items-center space-x-2">
         <Checkbox
-          id="translation"
-          checked={opts.translation}
-          onCheckedChange={() => toggle("translation")}
+          id="border"
+          checked={opts.border}
+          onCheckedChange={() => toggle("border")}
         />
-        <label htmlFor="translation">译文</label>
+        <label htmlFor="border">边框</label>
       </div>
 
       <div className="flex items-center space-x-2">
@@ -57,11 +57,11 @@ export const ToggleOption = () => {
 
       <div className="flex items-center space-x-2">
         <Checkbox
-          id="border"
-          checked={opts.border}
-          onCheckedChange={() => toggle("border")}
+          id="translation"
+          checked={opts.translation}
+          onCheckedChange={() => toggle("translation")}
         />
-        <label htmlFor="border">边框</label>
+        <label htmlFor="translation">译文</label>
       </div>
     </div>
   );
--- src/app/tools/print/page.tsx
+++ src/app/tools/print/page.tsx
@@ -1,16 +1,38 @@
 import { api } from "~/trpc/server";
 import PreviewPrint from "./components/preview-print";
+import { cache } from "react";
 
-export default async function Page({
-  searchParams,
-}: {
+interface Props {
   searchParams: { id: string };
-}) {
-  const { id } = searchParams;
+}
 
-  const poem = await api.poem.findById.query({
-    id: Number(id),
+const getItem = cache(async ({ searchParams }: Props) => {
+  if (searchParams.id === "null" || !searchParams.id) return null;
+
+  return api.poem.findById.query({
+    id: Number(searchParams.id),
   });
+});
+
+export async function generateMetadata(props: Props) {
+  const poem = await getItem(props);
+
+  if (!poem) {
+    return {
+      title: "诗词打印工具 | 现代化中国诗词学习网站",
+      description:
+        "本工具支持在线打印诗词田字格、内容、译文、拼音。允许你自由组合打印方式。",
+    };
+  }
+
+  return {
+    title: `诗词《${poem.title}》打印`,
+    description: `诗词《${poem.title}》作者${poem.author.name}，支持在线打印诗词田字格、内容、译文、拼音。允许你自由组合打印方式。`,
+  };
+}
+
+export default async function Page({ searchParams }: Props) {
+  const poem = await getItem({ searchParams });
 
   if (!poem) return null;
 
