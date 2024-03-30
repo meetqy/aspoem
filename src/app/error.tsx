69c04535ef74d40d50c14d6f7b47e21581c02124
meetqy <meetqy@qq.com>
2024-03-31 00:01:16 +0800

    not-found adn error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -2,21 +2,16 @@
 
 import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
-import { useParams } from "next/navigation";
 import { Button } from "~/components/ui/button";
 
 export default function NotFound() {
-  const params = useParams<{ lang: string }>();
-
-  const { lang = "zh-Hans" } = params;
-
   return (
     <div className="relative m-auto flex min-h-screen max-w-screen-md flex-col items-center justify-center">
       <div className="absolute top-[30%] text-center font-mono">
         <h1 className="prose-h1">Page Error !</h1>
         <p className="mt-12">
           <Button size={"lg"} asChild>
-            <Link href={`/${lang}`}>
+            <Link href={"/"}>
               <ArrowLeft className="mr-2" /> Go Back Home
             </Link>
           </Button>
