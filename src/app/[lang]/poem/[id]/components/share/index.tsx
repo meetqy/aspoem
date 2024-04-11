7955729cbda941124a0c338b038b72d18dfa7108
meetqy <meetqy@qq.com>
2024-04-11 13:23:48 +0800

    build image

--- src/app/[lang]/poem/[id]/components/share/index.tsx
+++ src/app/[lang]/poem/[id]/components/share/index.tsx
@@ -11,6 +11,50 @@ interface Props {
   scale?: number;
 }
 
+const buildPng = async (box: HTMLElement, scale: number) => {
+  let result = "";
+  const safari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
+  if (!safari) {
+    return await toPng(box, {
+      width: box.clientWidth * scale,
+      height: box.clientHeight * scale,
+      cacheBust: true,
+      style: {
+        transform: `scale(${scale})`,
+        transformOrigin: "top left",
+      },
+    });
+  }
+
+  const resultBytes: number[] = [];
+  let reBuild = true;
+
+  while (reBuild) {
+    const res = await toPng(box, {
+      width: box.clientWidth * scale,
+      height: box.clientHeight * scale,
+      cacheBust: resultBytes.length === 0,
+      style: {
+        transform: `scale(${scale})`,
+        transformOrigin: "top left",
+      },
+    });
+
+    if (
+      res.length !== resultBytes[resultBytes.length - 1] &&
+      resultBytes.length > 0
+    ) {
+      result = res;
+      reBuild = false;
+    }
+
+    resultBytes.push(res.length);
+  }
+
+  return result;
+};
+
 const SaveShareButton = (props: Props) => {
   let { scale = 1 } = props;
   const [visable, setVisable] = useState(false);
@@ -25,47 +69,49 @@ const SaveShareButton = (props: Props) => {
         const bgEl: HTMLElement = box.querySelector("#draw-share-card-bg")!;
 
         const url = bgEl.style.backgroundImage.replace(/url\("(.+)"\)/, "$1");
-        console.log();
 
-        toPng(box, {
-          width: box.clientWidth * scale,
-          height: box.clientHeight * scale,
-          cacheBust: true,
-          style: {
-            transform: `scale(${scale})`,
-            transformOrigin: "top left",
-          },
-        }).then((src) => {
-          if (!src) return;
+        const src = await buildPng(box, scale);
+        if (!src) return;
 
-          const div = document.createElement("div");
-          div.setAttribute(
-            "style",
-            `position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.25); z-index: 9998; display: flex; justify-content: center; align-items: center; flex-direction: column;`,
-          );
+        const div = document.createElement("div");
+        div.setAttribute(
+          "style",
+          `position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.25); z-index: 9998; display: flex; justify-content: center; align-items: center; flex-direction: column;`,
+        );
 
-          div.onclick = () => {
-            document.body.removeChild(div);
-          };
+        div.onclick = () => {
+          document.body.removeChild(div);
+        };
 
-          const img = new Image();
-          img.src = src;
-          img.style.height = "90%";
-          img.style.width = "auto";
-          // contain
-          img.style.objectFit = "contain";
+        const img = new Image();
+        img.src = src;
+        img.style.height = "90%";
+        img.style.width = "auto";
+        // contain
+        img.style.objectFit = "contain";
 
-          div.appendChild(img);
+        div.appendChild(img);
 
-          const span = document.createElement("span");
-          span.innerText = url;
-          div.appendChild(span);
-          span.style.color = "white";
+        const span = document.createElement("span");
+        span.innerText = url;
+        div.appendChild(span);
+        span.style.color = "white";
 
-          document.body.appendChild(div);
+        document.body.appendChild(div);
 
-          setVisable(false);
-        });
+        setVisable(false);
+
+        // toPng(box, {
+        //   width: box.clientWidth * scale,
+        //   height: box.clientHeight * scale,
+        //   cacheBust: true,
+        //   style: {
+        //     transform: `scale(${scale})`,
+        //     transformOrigin: "top left",
+        //   },
+        // }).then((src) => {
+
+        // });
       }
     })();
   }, [scale, visable]);
--- src/utils/bg-card.ts
+++ src/utils/bg-card.ts
@@ -4,7 +4,7 @@ const bgCardsImages = [
   { color: "white", url: "https://r2.aspoem.com/neutral-card-bg/3.jpg" },
   { color: "black", url: "https://r2.aspoem.com/neutral-card-bg/4.jpg" },
   { color: "white", url: "https://r2.aspoem.com/neutral-card-bg/5.jpg" },
-  { color: "white", url: "https://r2.aspoem.com/neutral-card-bg/6.jpg" },
+  { color: "black", url: "https://r2.aspoem.com/neutral-card-bg/6.jpg" },
   { color: "black", url: "https://r2.aspoem.com/neutral-card-bg/7.jpg" },
   { color: "black", url: "https://r2.aspoem.com/neutral-card-bg/8.jpg" },
   { color: "black", url: "https://r2.aspoem.com/neutral-card-bg/9.jpg" },
