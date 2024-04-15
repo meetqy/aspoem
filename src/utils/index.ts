ae6eb66b1c0cad5587723f80f834b2241f8c05e9
meetqy <meetqy@qq.com>
2024-04-15 19:47:21 +0800

    build error

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export * from "./bg-card";
-
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
