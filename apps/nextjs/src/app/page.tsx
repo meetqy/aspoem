7a69d9d8711a013abb0c0948401189a7e4f62209
meetqy <meetqy@icloud.com>
2024-05-05 11:54:21 +0800

    eslint

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,7 +1,13 @@
-export default function HomePage() {
+import { api } from "~/trpc/server";
+
+export default async function HomePage() {
+  const v = await api.post.hello();
+
+  console.log(v);
+
   return (
     <div>
-      <h1>Welcome to nextjs!</h1>
+      <h1>Welcome to {v}!</h1>
     </div>
   );
 }
--- packages/api/src/router/post.ts
+++ packages/api/src/router/post.ts
@@ -1,40 +1,12 @@
 import type { TRPCRouterRecord } from "@trpc/server";
 import { z } from "zod";
 
-import { desc, eq, schema } from "@aspoem/db";
 import { CreatePostSchema } from "@aspoem/validators";
 
 import { protectedProcedure, publicProcedure } from "../trpc";
 
 export const postRouter = {
-  all: publicProcedure.query(({ ctx }) => {
-    // return ctx.db.select().from(schema.post).orderBy(desc(schema.post.id));
-    return ctx.db.query.post.findMany({
-      orderBy: desc(schema.post.id),
-      limit: 10,
-    });
-  }),
-
-  byId: publicProcedure
-    .input(z.object({ id: z.number() }))
-    .query(({ ctx, input }) => {
-      // return ctx.db
-      //   .select()
-      //   .from(schema.post)
-      //   .where(eq(schema.post.id, input.id));
-
-      return ctx.db.query.post.findFirst({
-        where: eq(schema.post.id, input.id),
-      });
-    }),
-
-  create: protectedProcedure
-    .input(CreatePostSchema)
-    .mutation(({ ctx, input }) => {
-      return ctx.db.insert(schema.post).values(input);
-    }),
-
-  delete: protectedProcedure.input(z.number()).mutation(({ ctx, input }) => {
-    return ctx.db.delete(schema.post).where(eq(schema.post.id, input));
+  hello: publicProcedure.query(() => {
+    return "world";
   }),
 } satisfies TRPCRouterRecord;
