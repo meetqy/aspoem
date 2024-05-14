10195554f82a4f248d5da7960edc90aa7801bbe1
meetqy <meetqy@icloud.com>
2024-05-14 21:41:00 +0800

    init ct3a

---
+++ apps/ct3a/src/app/layout.tsx
@@ -0,0 +1,29 @@
+import "@/styles/globals.css";
+import { Inter as FontSans } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable,
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
