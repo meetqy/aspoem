b40ddd339f7d76a3ccacda0619b6f85a9eb505ed
meetqy <meetqy@icloud.com>
2024-05-14 22:06:04 +0800

    nextui

---
+++ apps/nextui/app/layout.tsx
@@ -0,0 +1,64 @@
+import "@/styles/globals.css";
+import { Metadata, Viewport } from "next";
+import { siteConfig } from "@/config/site";
+import { fontSans } from "@/config/fonts";
+import { Providers } from "./providers";
+import { Navbar } from "@/components/navbar";
+import { Link } from "@nextui-org/link";
+import clsx from "clsx";
+
+export const metadata: Metadata = {
+	title: {
+		default: siteConfig.name,
+		template: `%s - ${siteConfig.name}`,
+	},
+	description: siteConfig.description,
+	icons: {
+		icon: "/favicon.ico",
+	},
+};
+
+export const viewport: Viewport = {
+	themeColor: [
+		{ media: "(prefers-color-scheme: light)", color: "white" },
+		{ media: "(prefers-color-scheme: dark)", color: "black" },
+	],
+}
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<head />
+			<body
+				className={clsx(
+					"min-h-screen bg-background font-sans antialiased",
+					fontSans.variable
+				)}
+			>
+				<Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+					<div className="relative flex flex-col h-screen">
+						<Navbar />
+						<main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+							{children}
+						</main>
+						<footer className="w-full flex items-center justify-center py-3">
+							<Link
+								isExternal
+								className="flex items-center gap-1 text-current"
+								href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
+								title="nextui.org homepage"
+							>
+								<span className="text-default-600">Powered by</span>
+								<p className="text-primary">NextUI</p>
+							</Link>
+						</footer>
+					</div>
+				</Providers>
+			</body>
+		</html>
+	);
+}
---
+++ apps/nextui/app/page.tsx
@@ -0,0 +1,51 @@
+import { Link } from "@nextui-org/link";
+import { Snippet } from "@nextui-org/snippet";
+import { Code } from "@nextui-org/code"
+import { button as buttonStyles } from "@nextui-org/theme";
+import { siteConfig } from "@/config/site";
+import { title, subtitle } from "@/components/primitives";
+import { GithubIcon } from "@/components/icons";
+
+export default function Home() {
+	return (
+		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+			<div className="inline-block max-w-lg text-center justify-center">
+				<h1 className={title()}>Make&nbsp;</h1>
+				<h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
+				<br />
+				<h1 className={title()}>
+					websites regardless of your design experience.
+				</h1>
+				<h2 className={subtitle({ class: "mt-4" })}>
+					Beautiful, fast and modern React UI library.
+				</h2>
+			</div>
+
+			<div className="flex gap-3">
+				<Link
+					isExternal
+					href={siteConfig.links.docs}
+					className={buttonStyles({ color: "primary", radius: "full", variant: "shadow" })}
+				>
+					Documentation
+				</Link>
+				<Link
+					isExternal
+					className={buttonStyles({ variant: "bordered", radius: "full" })}
+					href={siteConfig.links.github}
+				>
+					<GithubIcon size={20} />
+					GitHub
+				</Link>
+			</div>
+
+			<div className="mt-8">
+				<Snippet hideSymbol hideCopyButton variant="flat">
+					<span>
+						Get started by editing <Code color="primary">app/page.tsx</Code>
+					</span>
+				</Snippet>
+			</div>
+		</section>
+	);
+}
