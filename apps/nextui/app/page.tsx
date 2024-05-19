0f1786843665191854f25a426a30eaf8b3de9b51
meetqy <meetqy@icloud.com>
2024-05-19 11:43:04 +0800

    123

--- apps/nextui/app/page.tsx
+++ apps/nextui/app/page.tsx
@@ -1,5 +1,12 @@
+import DismissableCard2 from "@/components/dismissable-card2";
 import ReviewCommentCard from "@/components/review-comment-card";
 
 export default function Page() {
-  return new Array(20).fill(0).map((_, i) => <ReviewCommentCard key={i} />);
+  return (
+    <div className="grid gap-4 px-4 mt-4">
+      {new Array(20).fill(0).map((_, i) => (
+        <DismissableCard2 key={i} />
+      ))}
+    </div>
+  );
 }
