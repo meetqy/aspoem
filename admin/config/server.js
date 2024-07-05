b69a775d551fbce034b0f0db142c32cb3d136124
meetqy <meetqy@icloud.com>
2024-07-05 10:46:11 +0800

    12

--- admin/config/server.js
+++ admin/config/server.js
@@ -1,10 +1,11 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
+  url: env("URL", "http://localhost:1337"),
 });
