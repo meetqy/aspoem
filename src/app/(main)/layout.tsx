5bc4936da784893056e0722f027e08aa03bbe2b1
meetqy <meetqy@icloud.com>
2025-10-29 14:50:12 +0800

    remove app-sidebar

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,18 +1,5 @@
-import { AppSidebar } from '@/components/app-sidebar'
-import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
-import { navMain } from '@/lib/nav-main'
-
-export default function Layout({ children, breadcrumb }: { children: React.ReactNode, breadcrumb: React.ReactNode }) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <SidebarProvider>
-      <AppSidebar items={navMain} />
-      <SidebarInset>
-        <header className="bg-background/95 sticky top-0 z-10 flex h-16 shrink-0 items-center gap-2 border-b px-4 backdrop-blur-sm">
-          <SidebarTrigger className="mr-3 -ml-1" />
-          {breadcrumb}
-        </header>
-        <main className="relative flex-1 p-4">{children}</main>
-      </SidebarInset>
-    </SidebarProvider>
+    children
   )
 }
