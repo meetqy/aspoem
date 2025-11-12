a242a1b3a0e39cfc75bda77c3494cfc7bbcd1f56
meetqy <meetqy@icloud.com>
2025-11-12 10:35:10 +0800

    refactor: simplify HomePage component by removing unused API query and ensuring redirect to /poems

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,7 @@
-"use client";
-
-import { api } from "@/trpc/react";
+import { redirect } from "next/navigation";
 
 // import { redirect } from "next/navigation";
 export default function HomePage() {
-  const { data } = api.poem.getRandom.useQuery();
-  // redirect("/poems");
+  redirect("/poems");
   return null;
 }
