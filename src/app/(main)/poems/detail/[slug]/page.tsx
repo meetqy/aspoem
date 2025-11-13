d10586b33c968bc1a15e810ab23ff650fa2a00a4
meetqy <meetqy@icloud.com>
2025-11-13 14:32:37 +0800

    feat: enhance PoemDetailPage with button tags and improved annotation rendering

--- src/app/(main)/poems/detail/[slug]/page.tsx
+++ src/app/(main)/poems/detail/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { PoemTypography } from "@/components/poem-typography";
+import { Button } from "@/components/ui/button";
 import { SidebarContent, SidebarProvider } from "@/components/ui/sidebar";
 import { api } from "@/trpc/server";
 import { SidebarRight } from "./_components/sidebar-right";
@@ -15,8 +16,45 @@ export default async function PoemDetailPage({
   return (
     <div className="flex flex-col flex-1">
       <SidebarProvider className="relative container-wrapper">
-        <SidebarContent>
+        <SidebarContent className="pb-12">
           <PoemTypography poem={poem} />
+
+          <div className="max-w-screen-md w-full mx-auto">
+            <div className="flex gap-2 flex-wrap">
+              {poem.tags.map((tag) => (
+                <Button variant={"secondary"} key={tag.slug}>
+                  {tag.name}
+                </Button>
+              ))}
+            </div>
+            <section className="prose font-sans max-w-none mt-8">
+              {poem.translation && (
+                <>
+                  <h2>译文</h2>
+                  <p
+                    dangerouslySetInnerHTML={{
+                      __html: poem.translation.replaceAll("\n", "<br/>"),
+                    }}
+                  />
+                </>
+              )}
+
+              {poem.annotation && (
+                <>
+                  <h2>注解</h2>
+                  <ul>
+                    {Object.entries(poem.annotation || {}).map(
+                      ([key, value]) => (
+                        <li key={key}>
+                          {key}：{value}
+                        </li>
+                      ),
+                    )}
+                  </ul>
+                </>
+              )}
+            </section>
+          </div>
         </SidebarContent>
 
         <SidebarRight poem={poem} />
--- src/components/poem-typography/index.tsx
+++ src/components/poem-typography/index.tsx
@@ -14,74 +14,46 @@ export const PoemTypography = ({ poem }: { poem: ApiPoemFindDetail }) => {
   return (
     <main
       className={cn(
-        "@container/poem mx-auto max-w-screen-md rounded-md p-4 w-full md:py-24 py-12",
+        "@container/poem mx-auto max-w-screen-md rounded-md p-4 w-full py-16",
         font === "sans" ? "font-sans" : "font-cursive",
       )}
       style={{
         fontSize: 20,
       }}
     >
-      <section>
-        <PoemTypographyTitle
-          text={poem.title}
-          pinyin={poem.titlePinyin}
+      <PoemTypographyTitle
+        text={poem.title}
+        pinyin={poem.titlePinyin}
+        font={font as "cursive" | "sans"}
+        pinyinVisible={pinyinVisible}
+      />
+
+      <div className={cn(pinyinVisible ? "mt-[1em]" : "mt-[0.5em]")}>
+        <PoemTypographyAuthor
+          dynasty={poem.dynasty?.name}
+          dynastyPinyin={poem.dynasty?.pinyin}
+          author={poem.author.name}
+          authorPinyin={poem.author.pinyin}
           font={font as "cursive" | "sans"}
           pinyinVisible={pinyinVisible}
         />
+      </div>
 
-        <div className={cn(pinyinVisible ? "mt-[1em]" : "mt-[0.5em]")}>
-          <PoemTypographyAuthor
-            dynasty={poem.dynasty?.name}
-            dynastyPinyin={poem.dynasty?.pinyin}
-            author={poem.author.name}
-            authorPinyin={poem.author.pinyin}
-            font={font as "cursive" | "sans"}
-            pinyinVisible={pinyinVisible}
-          />
-        </div>
-
-        <div
-          className={cn(
-            "transition-all",
-            pinyinVisible ? "mt-[2em]" : "mt-[1em]",
-          )}
-        >
-          <PoemTypographyContent
-            paragraphs={poem.paragraphs}
-            paragraphsPinyin={poem.paragraphsPinyin}
-            annotation={poem.annotation as Record<string, string>}
-            font={font as "cursive" | "sans"}
-            pinyinVisible={pinyinVisible}
-            annotationVisible={annotationVisible}
-          />
-        </div>
-      </section>
-
-      <section className="prose font-sans mt-24">
-        {poem.translation && (
-          <>
-            <h2>译文</h2>
-            <p
-              dangerouslySetInnerHTML={{
-                __html: poem.translation.replaceAll("\n", "<br/>"),
-              }}
-            />
-          </>
+      <div
+        className={cn(
+          "transition-all",
+          pinyinVisible ? "mt-[2em]" : "mt-[1em]",
         )}
-
-        {poem.annotation && (
-          <>
-            <h2>注解</h2>
-            <ul>
-              {Object.entries(poem.annotation || {}).map(([key, value]) => (
-                <li key={key}>
-                  {key}：{value}
-                </li>
-              ))}
-            </ul>
-          </>
-        )}
-      </section>
+      >
+        <PoemTypographyContent
+          paragraphs={poem.paragraphs}
+          paragraphsPinyin={poem.paragraphsPinyin}
+          annotation={poem.annotation as Record<string, string>}
+          font={font as "cursive" | "sans"}
+          pinyinVisible={pinyinVisible}
+          annotationVisible={annotationVisible}
+        />
+      </div>
     </main>
   );
 };
