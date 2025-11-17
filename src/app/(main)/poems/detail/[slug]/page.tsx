263fe0a1d578d30e5f461e97b7d30df5f97b70d0
meetqy <meetqy@icloud.com>
2025-11-17 10:02:31 +0800

    fix: update last modified timestamp display in poem detail page

--- src/app/(main)/poems/detail/[slug]/page.tsx
+++ src/app/(main)/poems/detail/[slug]/page.tsx
@@ -80,11 +80,9 @@ export default async function PoemDetailPage({
                   />
                 </>
               )}
-            </section>
 
-            <section className="">
-              <hr />
-              <p className="text-muted-foreground text-right text-sm mt-4">
+              <hr className="mb-4" />
+              <p className="text-muted-foreground text-right text-sm">
                 最后更新：
                 <time suppressHydrationWarning>
                   {new Date(poem.updatedAt).toLocaleString("zh-CN")}
